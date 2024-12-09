9f43fa056f96102cc1d06b6c342ce9ef1f2c3e72
Ваня <@DESKTOP-1L36HFU>
2024-12-10 00:06:35 +0200

    Add project files.

---
+++ lab_2 Городиський_Іван_724а.cpp
@@ -0,0 +1,12 @@
+﻿
+#include <iostream>
+#include <math.h>
+using namespace std;
+double z1,z2,b;
+int main() {
+	cin >> b;
+	z1 = (sqrt((2 * b) + (2 * (sqrt(pow(b, 2) - 4))))) / (sqrt(pow(b, 2) - 4) + b + 2);
+	z2 = 1.0 / (sqrt(b + 2.0));
+	cout << "z1 = " << z1 << "\n";
+	cout << "z2 = " << z2;
+}
